ed933df38232309246bdd9505a2b7c02a677fb8f
Aniket Pandey <aniketp@iitk.ac.in>
2017-06-20 21:33:29 +0530

    Added DRF made Restful api

---
+++ DRF/api/views.py
@@ -0,0 +1,19 @@
+from rest_framework import generics, permissions
+from .serializers import BucketlistSerializer
+from .permissions import IsOwner
+from .models import Bucketlist
+
+
+class CreateView(generics.ListCreateAPIView):
+    queryset = Bucketlist.objects.all()
+    serializer_class = BucketlistSerializer
+    permission_classes = (permissions.IsAuthenticated, IsOwner)
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class DetailsView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Bucketlist.objects.all()
+    serializer_class = BucketlistSerializer
+    permission_classes = (permissions.IsAuthenticated, IsOwner)
